45ae35ba28825103503782aa451d83405dcb0cc1
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-10 21:24:03 +0530

    UC6 completed...

--- Attendance.java
+++ Attendance.java
@@ -1,35 +1,40 @@
 package org.example;
+
 import java.util.Random;
 
+import static java.lang.System.exit;
+
 public class Attendance {
+    final private int isAbsent = 0;
+    final int worked_full_time = 1;
+    final int worked_part_time = 2;
     int totalWage = 0;
     final int wage_per_hour = 20;
     final int full_day_hour = 8;
     final int part_time_hour = 4;
     int day = 1;
-    int month = 20;
+    int total_hour = 0;
     Random rd = new Random();
 
     public int checkAttendance() {
-        while (day <= month) {
-            int toCheck = rd.nextInt(3);
-            day++;
-            switch (toCheck) {
-                case 0:
-                    wageCalculator(0);
-                    break;
-                case 1:
-                    wageCalculator(full_day_hour);
-                    break;
-                default:
-                    wageCalculator(part_time_hour);
+        while (day < 20 && total_hour < 100) {
+//
+            int work = rd.nextInt(3);
+            if (work == worked_full_time) {
+                day++;
+                wageCalculator(full_day_hour *  wage_per_hour);
             }
+            if (work == worked_part_time) {
+                total_hour += part_time_hour;
+                wageCalculator(part_time_hour * wage_per_hour);
+            }
+//            System.out.println(day + " " + total_hour);
         }
-        return  totalWage;
+        return totalWage;
     }
 
-    public void wageCalculator(int attendance) {
-        int dailyWage = attendance * wage_per_hour;
-        totalWage += dailyWage;
+    public void wageCalculator(int daily_wage) {
+        totalWage += daily_wage;
+//        System.out.println("Total Wagesss = " + totalWage);
     }
 }
